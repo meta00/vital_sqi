de05114b3b9d9f8b97d9d9d45608ae1e03e4b8eb
meta00 <hobichhai@gmail.com>
2021-03-29 17:15:58 +0700

    modify install requirement

--- setup.py
+++ setup.py
@@ -1,19 +1,21 @@
 #!/usr/bin/python3
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 with open('README.md') as f:
     long_description = f.read()
 
 setup(
-    name='vital_sqi',
-    version='0.0.1',
-    description="Signal quality control pipeline for electrocardiogram and photoplethysmogram",
-    long_description=long_description,
-    author='Hai Ho, Khoa Le', # alphabetical order
-    author_email='haihb@oucru.org, khoaldv@oucru.org',
-    py_modules=['common', 'data', 'preprocess', 'sqi'],
-    install_requires=['numpy',
+    name = 'vital_sqi',
+    version = '0.1.0',
+    packages = find_packages(include = ["vital_sqi", "vital_sqi.*"]),
+    description = "Signal quality control pipeline for electrocardiogram and "
+                 "photoplethysmogram",
+    long_description = long_description,
+    author='Hai Ho, Khoa Le',
+    author_email = 'haihb@oucru.org, khoaldv@oucru.org',
+    py_modules = ['common', 'data', 'preprocess', 'sqi'],
+    install_requires = ['numpy',
                       'matplotlib',
                       'scipy',
                       'sklearn',
@@ -22,10 +24,10 @@ setup(
                       'plotly',
                       'dtw-python',
                       'py-ecg-detectors'],
-    zip_safe=False,
-    url='https://github.com/meta00/vital_sqi',
-    license='GPL 3.0',
-    classifiers=[
+    zip_safe = False,
+    url = 'https://github.com/meta00/vital_sqi',
+    license = 'GPL 3.0',
+    classifiers = [
         'Intended Audience :: Developers',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 3',
