a5750f0f43ab34a3e57349b18970a7aa1a33f0f9
meta00 <hobichhai@gmail.com>
2021-04-08 11:49:15 +0700

    check valid signal function

---
+++ vital_sqi/common/utils.py
@@ -0,0 +1,31 @@
+import numpy as np
+def check_valid_signal(x):
+    """
+    Check whether signal is valid, i.e. an array_like numeric, or raise errors.
+
+    Parameters
+    ----------
+    x :
+        array_like, array of signal
+
+    Returns
+    -------
+    type
+        bool, boolean indicating if the signal is valid
+
+    """
+    if len(x) == 0:
+        raise ValueError("Empty signal")
+    types = []
+    for i in range(len(x)):
+        types.append(str(type(x[i])))
+    type_unique = np.unique(np.array(types))
+    if len(type_unique) != 1 and \
+            type_unique[0].find("int") == -1 and \
+            type_unique[0].find("float") == -1:
+        raise ValueError("Expect numeric array, instead found array with "
+                         "types {0}: ".format(type_unique))
+    if type_unique[0].find("int") == -1 and type_unique[0].find("float") == -1:
+        raise ValueError("Expect numeric array, instead found array with "
+                         "types {0}: ".format(type_unique))
+    return True
