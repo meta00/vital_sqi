3333382473b2baa36838a61ffb52089fb12c6a38
meta00 <hobichhai@gmail.com>
2021-04-08 11:48:40 +0700

    add test to dtw_sqi

--- tests/sqi/test_dtw_sqi.py
+++ tests/sqi/test_dtw_sqi.py
@@ -1,8 +1,26 @@
 import pytest
-import numpy as np
-from dtw import dtw
-from vital_sqi.common.generate_template import ppg_absolute_dual_skewness_template, \
-        ppg_dual_double_frequency_template,ppg_nonlinear_dynamic_system_template
+
+
+from vital_sqi.sqi.dtw_sqi import dtw_sqi
+
+
 class TestDtwSqi(object):
-    def test_on_sth(self):
-        pass
+    x = [0, 1, 2, 3]
+
+    def test_on_invalid_template_type(self):
+        template_types = [2.1, 4]
+        for i in template_types:
+            with pytest.raises(ValueError) as exc_info:
+                dtw_sqi(self.x, i)
+            assert exc_info.match("Invalid template type")
+
+    def test_on_valid_template_type_0(self):
+        template_types = [0, 1, 2, 3]
+        for i in template_types:
+            assert type(dtw_sqi(self.x, i)) is float
+
+    def test_on_trace_equal_0(self):
+        x = [0]
+        template_types = [0, 1, 2, 3]
+        for i in template_types:
+            assert type(dtw_sqi(x, i)) is float
