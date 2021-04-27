4e464b5cf0598929e21828925899e41b7eeb333a
meta00 <hobichhai@gmail.com>
2021-04-27 17:23:02 +0700

    resolve conflict

--- tests/data/__init__.py
+++ tests/data/__init__.py
@@ -12,6 +12,5 @@ from vital_sqi.data.removal_utilities import (
 	cut_by_frequency_partition
 )
 from vital_sqi.data.segment_split import (
-	split_to_subsegments
+	split_by_time
 	)
-from vital_sqi.data.signal_io import *
