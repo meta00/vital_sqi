ad85acbd916eddc683b6925760e5909609229f97
Koaha <ldvankhoa@gmail.com>
2021-05-04 16:22:52 +0700

    add hrv_sqi

---
+++ tests/sqi/test_hrv_sqi.py
@@ -0,0 +1,63 @@
+import pytest
+import numpy as np
+
+class TestSdnnSqi(object):
+    def test_on_sdnn_sqi(self):
+        pass
+class TestSdsdSqi(object):
+    def test_on_sdsd_sqi(self):
+        pass
+class TestRmssdSqi(object):
+    def test_on_rmssd_sqi(self):
+        pass
+class TestCvsdSqi(object):
+    def test_on_cvsd_sqi(self):
+        pass
+class TestCvnnSqi(object):
+    def test_on_cvnn_sqi(self):
+        pass
+class TestMeanNnSqi(object):
+    def test_on_mean_nn_sqi(self):
+        pass
+class TestMedianNnSqi(object):
+    def test_on_median_nn_sqi(self):
+         pass
+class TestPnn50Sqi(object):
+    def test_on_pnn_50_sqi(self):
+         pass
+class TestPnn20Sqi(object):
+    def test_on_pnn_20_sqi(self):
+         pass
+class TestHrMeanSqi(object):
+    def test_on_hr_mean_sqi(self):
+         pass
+class TestHrMinSqi(object):
+    def test_on_hr_min_sqi(self):
+         pass
+class TestHrMaxSqi(object):
+    def test_on_hr_max_sqi(self):
+         pass
+class TestHrStdSqi(object):
+    def test_on_hr_std_sqi(self):
+         pass
+class TestPeakFrequencySqi(object):
+    def test_on_peak_frequency_sqi(self):
+         pass
+class TestAbsolutePowerSqi(object):
+    def test_on_absolute_power_sqi(self):
+         pass
+class TestLogPowerSqi(object):
+    def test_on_log_power_sqi(self):
+         pass
+class TestRelativePowerSqi(object):
+    def test_on_relative_power_sqi(self):
+         pass
+class TestNormalizedPowerSqi(object):
+    def test_on_normalized_power_sqi(self):
+         pass
+class TestLfHfRatioSqi(object):
+    def test_on_lf_hf_ratio_sqi(self):
+         pass
+class TestPoincareFeaturesSqi(object):
+    def test_on_poincare_features_sqi(self):
+         pass
