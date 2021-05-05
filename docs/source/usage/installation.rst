a399758324894460fec303035eb03f44d8db3ea0
bahp <bernard.hernandez.perez@gmail.com>
2021-05-05 18:18:02 +0100

    ~ initial sphinx setup for documentation

---
+++ docs/source/usage/installation.rst
@@ -0,0 +1,9 @@
+Installation
+============
+
+Install the package from the pypi repository
+
+.. code::
+
+    python -m pip install vital_sqi          # Install
+
---
+++ examples/tutorials/plot_ecg_pipeline.py
@@ -0,0 +1,9 @@
+"""
+ECG Pipeline
+====================
+
+.. warning:: Include code to show how to create a pipeline to
+             clean an ecg signal. It is probably in one of the
+             jupyter notebooks (maybe ecg_qc.ipynb).
+
+"""
