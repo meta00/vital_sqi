a2121110776addbdf7b274a1bdb1aee58df38185
Koaha <ldvankhoa@gmail.com>
2021-05-05 10:05:21 +0700

    fix the input of power spectral calculation from hr to rr interval and add unit test file

---
+++ tests/common/test_power_spectrum.py
@@ -0,0 +1,20 @@
+import pytest
+
+class TestCalculatePower(object):
+    def test_on_calculate_power(self):
+        pass
+class TestGetInterpolatedData(object):
+    def test_on_get_interpolated_data(self):
+        pass
+class TestGetTimeAndBpm(object):
+    def test_on_time_and_bpm(self):
+        pass
+class TestCalculatePsd(object):
+    def test_on_calculate_psd(self):
+        pass
+class TestCalculateSpectrogram(object):
+    def test_on_calculate_spectrogram(self):
+        pass
+class TestPowerWavelet(object):
+    def test_on_calculate_power_wavelet(self):
+        pass
