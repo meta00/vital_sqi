d0f2c93f997a811823709b995770d6c53c280f7c
meta00 <hobichhai@gmail.com>
2021-05-18 16:31:14 +0700

    generate ruleset and tests

--- vital_sqi/rule/generate_ruleset.py
+++ vital_sqi/rule/generate_ruleset.py
@@ -3,6 +3,8 @@ Class RuleSet contains rules of SQIs and their splitting orders, used to
 build a decision flow/tree.
 """
 from vital_sqi.rule.rule_class import Rule
+from pyflowchart import *
+import pandas as pd
 
 
 class RuleSet:
@@ -16,31 +18,73 @@ class RuleSet:
             if not isinstance(value, dict):
                 raise AttributeError('Rule set must be of dict type.')
             order = list(value.keys())
-            if not all(isinstance(order, int)):
-                raise ValueError('Order must be of type int')
+            for i in order:
+                if not isinstance(i, int):
+                    raise ValueError('Order must be of type int')
             if sorted(order) != list(range(min(order), max(order) + 1)):
                 raise ValueError('Order must contain consecutive numbers')
-            if order[0] != 1:
+            if sorted(order)[0] != 1:
                 raise ValueError('Order must start with 1.')
-            if not all(isinstance(order, Rule)):
-                raise ValueError('Rules must be of class Rule')
+            rs = list(value.values())
+            for i in rs:
+                if not isinstance(i, Rule):
+                    raise ValueError('Rules must be of class Rule')
         super().__setattr__(name, value)
 
-    def plot_tree(self):
-        return
-
     def export_rules(self):
-        return
+        rules = self.rules
+        st = StartNode('')
+        e = EndNode('')
+        ops = []
+        conds = []
+        for key, value in rules.items():
+            ops.append(OperationNode(value.name))
+            conds.append(ConditionNode(value.write_rule()))
+        # define the direction the connection will leave the node from
+        st.connect(ops[0])
+        for i in range(len(ops)):
+            ops[i].connect(conds[i])
+            conds[i].connect_no(e)
+            if i < len(ops) - 1:
+                conds[i].connect_yes(ops[i+1])
+        conds[-1].connect_yes(e)
+        fc = Flowchart(st)
+        print(fc.flowchart())
+        return True
 
-    def execute(self, x):
+    def execute(self, value_df):
+        assert isinstance(value_df, pd.DataFrame), \
+            'Expected data frame, found {0}'.format(type(value_df))
+        assert len(value_df) == 1, 'Expected data frame of 1 row but got {0}' \
+                                   'instead'.format(len(value_df))
         rules = self.rules
         rules = rules.items()
         rules = sorted(rules)
         decision = 'accept'
-        for i in rules:
-            rule = rules[i][1]
-            decision = rule.apply_rule(x)
+        for r in rules:
+            rule = r[1]
+            try:
+                value = value_df.iloc[0][rule.name]
+            except:
+                raise KeyError('SQI {0} not found in rule set'.format(value))
+            decision = rule.apply_rule(value)
             if decision == 'reject':
                 break
+        print(decision)
         return decision
 
+# r1 = Rule("sqi1")
+# r2 = Rule("sqi2")
+# r3 = Rule("sqi3")
+# import os
+# source = os.path.abspath('/Users/haihb/Documents/Work/Oucru/innovation'
+#                            '/vital_sqi/tests/test_data/rule_dict_test.json')
+# r1.load_def(source)
+# r2.load_def(source)
+# r3.load_def(source)
+# r = {'3' : r1, 2 : r2, 1 : r3}
+# s = RuleSet(r)
+# s.export_rules()
+# dat = [6, 100, 0]
+# dat = pd.DataFrame([dat], columns = ['sqi1', 'sqi2', 'sqi3'])
+# s.execute(dat)
