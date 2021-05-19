c46863217de77a92e904c20177c466e44e0972ea
meta00 <hobichhai@gmail.com>
2021-05-19 13:24:04 +0700

    update class

--- vital_sqi/data/signal_sqi_class.py
+++ vital_sqi/data/signal_sqi_class.py
@@ -1,98 +1,44 @@
 """
 Class containing signal, header and sqi
 """
+import numpy as np
+import pandas as pd
+import datetime as dt
 
 
 class SignalSQI:
     """ """
     def __init__(self, wave_type=None, signals=None, sampling_rate=None,
-                 start_datetime=None, sqi_indexes=None, info=None):
+                 start_datetime=None, info=None, sqis=None, segments=None):
         self.signals = signals
         self.sampling_rate = sampling_rate
         self.start_datetime = start_datetime
         self.wave_type = wave_type
-        self.sqi_indexes = sqi_indexes
         self.info = info
+        self.sqis = sqis
+        self.segments = segments
 
-    def update_info(self, info):
-        """
+    def __setattr__(self, name, value):
+        if name == 'wave_type':
+            assert value == 'ecg' or value == 'ppg', \
+                'Expected either ecg or ppg.'
+        if name == 'signals':
+            assert isinstance(value, np.ndarray), 'Expected signals to be ' \
+                                                  'numpy array, with one ' \
+                                                  'channel per column.'
+        if name == 'sampling_rate':
+            assert isinstance(value, int), 'Expected int'
+        if name == 'start_datetime':
+            assert isinstance(value, str) or \
+                   isinstance(value, dt.datetime) or \
+                   isinstance(value, dt.date), 'Expected str or datetime ' \
+                                               'object'
+        if name == 'sqis':
+            assert isinstance(value, np.ndarray) or \
+                   isinstance(value,pd.DataFrame), 'Expected SQI table as ' \
+                                                   'array or data frame'
+        if name == 'segments':
+            assert isinstance(value, list), 'Expected a list of signal ' \
+                                            'segments'
 
-        Parameters
-        ----------
-        info :
-            
-
-        Returns
-        -------
-        object of SignalSQI class
-        
-        """
-        self.info = info
-        return self
-
-    def update_signal(self, signals):
-        """
-
-        Parameters
-        ----------
-        signals : numpy.ndarray of shape (m, n)
-        m is the number of rows and n is the number of channels of the signal.
-            
-
-        Returns
-        -------
-        object of class SignalSQI
-        
-        """
-        self.signals = signals
-        return self
-
-    def update_sqi_indexes(self, sqi_indexes):
-        """
-
-        Parameters
-        ----------
-        sqi_indexes : numpy.ndarray of shape (m, n)
-        m is the number of signal segments, n is the number of SQIs.
-            
-
-        Returns
-        -------
-        object of class SignalSQI
-        
-        """
-        self.sqi_indexes = sqi_indexes
-        return self
-
-    def update_sampling_rate(self, sampling_rate):
-        """
-
-        Parameters
-        ----------
-        sampling_rate : float
-        Note: sampling_rate must be correct to reliably infer RR intervals,
-        etc.
-
-            
-
-        Returns
-        -------
-        object of class SignalSQI
-        """
-        self.sampling_rate = sampling_rate
-        return self
-
-    def update_start_datetime(self, start_datetime):
-        """
-
-        Parameters
-        ----------
-        start_datetime : datetime
-        start date and
-
-        Returns
-        -------
-        object of si
-        """
-        self.start_datetime = start_datetime
-        return self
+        super().__setattr__(name, value)
