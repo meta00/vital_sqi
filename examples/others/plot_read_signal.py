2df59c3b632e9e004ca021a1677339bcc75e73ae
bahp <bernard.hernandez.perez@gmail.com>
2021-06-11 19:19:17 +0100

    ~ including sqis in example

--- examples/others/plot_read_signal.py
+++ examples/others/plot_read_signal.py
@@ -20,9 +20,21 @@ import matplotlib.pyplot as plt
 # Scipy
 from scipy.stats import skew
 from scipy.stats import kurtosis
+from scipy.stats import entropy
+
+# Heartpy
+import heartpy as hp
 
 # vitalSQI
 from vital_sqi.data.signal_io import ECG_reader
+
+# ----------------------------
+# Constant
+# ----------------------------
+# Constant to define whether we are using the terminal in
+# order to plot the dataframes. Keep to false by default
+# specially when building the documentation.
+TERMINAL = True
 
 
 # ----------------------------
@@ -44,7 +56,7 @@ print(data.wave_type)
 print(data.sqi_indexes)
 print(data.info)
 
-
+fs = data.sampling_rate
 
 #################################################################
 # Formatting
@@ -75,7 +87,7 @@ signals = signals.reset_index()
 #          the device was disconnected and connected again, ...
 # Create timedelta
 signals['timedelta'] = \
-    pd.to_timedelta(signals.index / data.sampling_rate, unit='s')
+    pd.to_timedelta(signals.index / fs, unit='s')
 
 # Create datetimes (if needed)
 #signals['date'] = pd.to_datetime(data.start_datetime)
@@ -105,6 +117,10 @@ signals[1].plot(ax=axes[1])
 #################################################################
 # Preprocessing
 # -------------
+# This is more about the general preprocessing of the signal,
+# if there are specific preprocessing steps to generate
+# specific vital signals (e.g. hr) they will be implemented on
+# their own in the next step.
 
 ####################################
 # Lets trim the first/last 5 minutes
@@ -146,6 +162,34 @@ print(signals)
 
 
 
+#################################################################
+# Let's compute other signals (vitals) from raw data
+#
+# For instance, some quality index might be derived from signals
+# such as the hear rate (hr) based on ecg signals by detection
+# of peaks. Thus, their own methods can be specified to generate
+# the signal from the raw data.
+
+def hr(s):
+    return np.random.randint(low=40, high=150, size=s.shape)
+
+def rr(s):
+    return np.random.randint(low=15, high=25, size=s.shape)
+
+def bbpf(s):
+    """Butter Band Pass Filter"""
+    from vital_sqi.preprocess.band_filter import BandpassFilter
+    f = BandpassFilter(band_type='butter', fs=fs)
+    aux = f.signal_highpass_filter(s, cutoff=1, order=1)
+    aux = f.signal_lowpass_filter(s, cutoff=20, order=4)
+    return aux
+
+# Add vital signals
+signals['0_hr'] = hr(signals[0])
+signals['0_rr'] = rr(signals[0])
+signals['0_bbpf'] = bbpf(signals[0])
+
+
 
 
 #################################################################
@@ -165,8 +209,84 @@ print(signals)
 #          package, to have a series of sqi techniques very
 #          easy to compute. Also it would be great if they can
 #          be made compatible with pandas.
+#
+# .. note: I am creating a function so that the final column
+#          has the name of the function instead of the long
+#          name of the sqi function.
+#
+# .. note: The following sqi indexes were giving errors:
+#
+#     - ValueError: Can only compare identically-labeled Series object.
+#         sq.standard_sqi.zero_crossings_rate_sqi(x)
+#         sq.standard_sqi.mean_crossing_rate_sqi(x)
+#
+#     - NameError: name PeakDetector is not defined.
+#         sq.standard_sqi.msq_sqi(x)
+#
+#     - AttributeError: module 'vital_sqi.sqi' has no attribute 'rpeaks_sqi'
+#         sq.rpeaks_sqi.correlogram_sqi(x)
+#
+#        .. note: Implementation issue
+#         manually handled exception: name 'isscalar' is not defined warning.
+#
+#    - AttributeError: module 'vital_sqi.sqi.standard_sqi' has no attribute 'per_beat_sqi'
+
+# Library
+import vital_sqi.sqi as sq
+
 def own(x):
+    """Own defined SQI (randint)"""
     return np.random.randint(100)
+
+def own2(x):
+    """Own defined SQI (fixed array)"""
+    return [1,2]
+
+def snr(x, axis=0, ddof=0):
+    """Signal to noise ratio"""
+    a = np.asanyarray(x)
+    m = a.mean(axis)
+    sd = a.std(axis=axis, ddof=ddof)
+    return np.where(sd == 0, 0, m/sd)
+    #return np.mean(sq.standard_sqi.signal_to_noise_sqi(x))
+
+def zcr(x):
+    """Zero crossing rate"""
+    return 0.5 * np.mean(np.abs(np.diff(np.sign(x))))
+    #return sq.standard_sqi.zero_crossings_rate_sqi(x)
+
+def mcr(x):
+    """Mean crossing rate"""
+    return zcr(x - np.mean(x))
+    #return sq.standard_sqi.mean_crossing_rate_sqi(x)
+
+def msq(x):
+    """Mean SQ"""
+    return sq.standard_sqi.msq_sqi(x)
+
+def corr(x):
+    """Correlogram"""
+    a = sq.rpeaks_sqi.correlogram_sqi(x)
+
+def dtw(x):
+    """
+
+    .. note: Implementation issue
+        manually handled exception: name 'isscalar' is not defined warning.
+
+    .. note: Implementation issue
+        AttributeError: module 'vital_sqi.sqi.standard_sqi' has no attribute 'per_beat_sqi'
+    """
+    from vital_sqi.common.rpeak_detection import PeakDetector
+    detector = PeakDetector()
+    peak_list, trough_list = \
+        detector.ppg_detector(x, 7)
+    dtw_list = sq.standard_sqi.per_beat_sqi(\
+        sqi_func=sq.dtw_si, troughs=trough_list,
+        signal=x, taper=True, template_type=1
+    )
+    return np.mean(dtw_list)
+
 
 # .. note: What if it is a complex SQI that requires first
 #          to compute the peaks and then apply some numpy
@@ -188,8 +308,10 @@ def own(x):
 sqis = signals \
     .groupby(pd.Grouper(freq='30s')) \
     .agg({'idx': ['first', 'last'],
-          0: [skew, kurtosis, own],
-          1: [skew, kurtosis, own]})
+          0: [skew, kurtosis, snr, mcr],
+          1: [skew, kurtosis, snr, mcr, own, own2],
+          '0_bbpf': [zcr, msq], #dtw, corr]
+          })
 
 
 # .. note: We are assuming that the whole signal has been
@@ -200,8 +322,10 @@ sqis['w'] = np.arange(sqis.shape[0])
 
 # Show
 print("\nSQIs (all):")
-#print(sqis)
 sqis
+
+if TERMINAL:
+    print(sqis)
 
 ##########################################
 # Lets apply some signal quality rules
@@ -231,6 +355,9 @@ sqis = sqis[sqis.keep]
 # Show
 print("\nSQIs (valid):")
 sqis
+
+if TERMINAL:
+    print(sqis)
 
 
 
@@ -269,6 +396,9 @@ result = pd.concat(slices)
 # Show
 print("\nSignals (for valid sqis)")
 result
+
+if TERMINAL:
+    print(sqis)
 
 
 #########################################################
