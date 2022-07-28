567e641376ff172a85ee106b97b7381819f49696
Koaha <ldvankhoa@gmail.com>
2022-07-28 14:11:53 +0700

    app unit test

--- tests/app/test_app.py
+++ tests/app/test_app.py
@@ -1,9 +1,10 @@
 from dash.testing.application_runners import import_app
 from conftest import pytest_setup_options
+from selenium.webdriver import Chrome
 
 def test_homepage_app(dash_duo):
     app = import_app('vital_sqi.app.index')
-    # dash_duo.driver = ChromeDriverManager().install()
+    # dash_duo.driver = Chrome('./chromedriver',options=pytest_setup_options())
         # Remote(command_executor='http://selenium-hub:4444/wd/hub',
         #                      options=pytest_setup_options())
     dash_duo.start_server(app)
