681d3bae38f9651dccde383eb3ce9972b1602c91
Koaha <ldvankhoa@gmail.com>
2022-07-28 14:48:50 +0700

    app unit test remote host on travis-ci

--- tests/app/test_app.py
+++ tests/app/test_app.py
@@ -1,12 +1,13 @@
 from dash.testing.application_runners import import_app
 from conftest import pytest_setup_options
-from selenium.webdriver import Chrome
+from selenium.webdriver import Chrome,Remote
 
 def test_homepage_app(dash_duo):
     app = import_app('vital_sqi.app.index')
     # dash_duo.driver = ChromeDriverManager().install()
-    # dash_duo.driver = Chrome('./chromedriver',options=pytest_setup_options())
-        # Remote(command_executor='http://selenium-hub:4444/wd/hub',
-        #                      options=pytest_setup_options())
+    # Chrome('./chromedriver',options=pytest_setup_options())
+    dash_duo.driver = \
+        Remote(command_executor='http://localhost:9222',
+                             options=pytest_setup_options())
     dash_duo.start_server(app)
     assert dash_duo.get_logs() == [], "Browser console should contain no error"
