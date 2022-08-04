ed353025f3a7ebaf0eee92a426997e59b83ca58c
Koaha <ldvankhoa@gmail.com>
2022-08-04 14:37:21 +0700

    test_app_dashboard

--- tests/app/conftest.py
+++ tests/app/conftest.py
@@ -1,4 +1,5 @@
 from selenium.webdriver.chrome.options import Options
+import pytest
 
 # driver = Remote(command_executor='http://localhost:4444/wd/hub')
 def pytest_setup_options():
@@ -6,3 +7,14 @@ def pytest_setup_options():
     options.add_argument('--disable-gpu')
     options.add_argument('--headless')
     return options
+
+
+@pytest.fixture
+def unwrap():
+    def unwrapper(func):
+        if not hasattr(func, '__wrapped__'):
+            return func
+
+        return unwrapper(func.__wrapped__)
+
+    yield unwrapper
--- tests/app/test_app.py
+++ tests/app/test_app.py
@@ -1,18 +1,220 @@
 from dash.testing.application_runners import import_app
-from conftest import pytest_setup_options
-from selenium.webdriver import Chrome,Remote
-from webdriver_manager.chrome import ChromeDriverManager
+from vital_sqi.app.index import display_page,update_output
+from vital_sqi.app.views.dashboard1 import on_data_set_table as load_data_dashboard_1
+from vital_sqi.app.views.dashboard2 import on_data_set_table as load_data_dashboard_2
+from vital_sqi.app.views import dashboard2
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from selenium.webdriver import Chrome
+from selenium import webdriver
 import os
+from dash.testing.composite import DashComposite
+from time import sleep
+import pandas as pd
+import os
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from mock import Mock,MagicMock
+import json
 
-def test_homepage_app(dash_duo):
-    app = import_app('vital_sqi.app.index')
-    # dash_duo.driver = ChromeDriverManager().install()
-    # Chrome('./chromedriver',options=pytest_setup_options())
-    # if 'TRAVIS' in os.environ:
-    #     dash_duo.driver = ChromeDriverManager().install()
-        # dash_duo.driver = ('./chromedriver')
-        # dash_duo.driver = \
-        #     Remote(command_executor='http://localhost:9222',
-        #                          options=pytest_setup_options())
-    dash_duo.start_server(app)
-    assert dash_duo.get_logs() == [], "Browser console should contain no error"
+
+# def test_onload_app(dash_duo):
+#     app = import_app('vital_sqi.app.index')
+#     dash_duo.start_server(app)
+#     assert dash_duo.get_logs() == [], "Browser console should contain no error"
+#
+#
+# def test_load_dashboard1(dash_duo):
+#     dashboard1_content = display_page('/views/dashboard1')
+#     assert dashboard1_content.children is not None
+#
+#
+# def test_load_dashboard2(dash_duo):
+#     dashboard2_content = display_page('/views/dashboard2')
+#     assert dashboard2_content.children is not None
+#
+#
+# def test_load_dashboard3(dash_duo):
+#     dashboard3_content = display_page('/views/dashboard3')
+#     assert dashboard3_content.children is not None
+#
+#
+# def test_upload_data(dash_duo):
+#     homepage_content = display_page('/homepage')
+#     mock_app = dash.Dash(__name__)
+#     mock_app.layout = html.Div(id='dashboard-content',children=[
+#         dcc.Store(id="dataframe", storage_type='local'),
+#         homepage_content
+#     ])
+#     dash_duo.start_server(mock_app)
+#     driver = dash_duo.driver
+#     mock_filename = 'mock_sqis.csv'
+#     upload_data_field = \
+#                 driver.find_element(
+#                 by=By.XPATH,
+#                 value='//*[@id="upload-data"]/div/input')
+#
+#     upload_data_field.\
+#         send_keys(os.path.join(os.getcwd(),mock_filename))
+#
+#     assert True
+#
+#
+# def test_on_data_set_table(dash_duo):
+#     mock_data = pd.read_csv('mock_sqis.csv')
+#     mock_content_db1 = load_data_dashboard_1(mock_data)
+#     assert mock_content_db1 is not None
+
+
+def test_dashboard_2(dash_duo):
+    mock_app = import_app('vital_sqi.app.views.dashboard2')
+    mock_data = pd.read_csv('mock_sqis.csv')
+    mock_data_rule = {
+        "skewness_sqi": {
+            "name": "skewness_sqi",
+            "def": [
+                {"op": ">=", "value": "10", "label": "reject"},
+                {"op": ">=", "value": "3", "label": "accept"},
+                {"op": "<", "value": "3", "label": "reject"}],
+            "desc": "",
+            "ref": ""
+        }
+    }
+    mock_content_db2 = load_data_dashboard_2(mock_data, mock_data_rule)
+    mock_app.layout = dashboard2.layout
+    # mock_app.layout = import_app('vital_sqi.app.views.dashboard2').layout
+    mock_app.layout.children[2] = mock_content_db2[0]
+
+    dash_duo.start_server(mock_app)
+    driver = dash_duo.driver
+    driver.get(dash_duo.server_url)
+
+    print("=================== get layout ======================")
+    # assert dash_duo.find_element('#confirm-rule-button').text == "Confirm"
+    # print("=================== get layout children======================")
+    mock_confirmed_button = \
+        driver.find_element(
+            by=By.ID,
+            value='confirm-rule-button')
+
+    mock_switch_selection = driver.find_element(
+            by=By.ID,
+            value='switch-selection')
+    mock_switch_selection.send_keys(True)
+    mock_confirmed_button.click()
+    print(dash_duo.get_logs() == [])
+    # assert dash_duo.get_logs() == [], "Click got Error"
+
+
+# def test_upload_data(dash_duo):
+#     app = import_app('vital_sqi.app.index')
+#     # home_content = display_page('home_content')
+#     # temp_layout = app.layout
+#     # temp_layout_children = temp_layout.children
+#     # temp_layout_children[5] = home_content
+#     # temp_layout.children = temp_layout_children
+#     # app.layout = temp_layout
+#
+#     driver = dash_duo.driver
+#     dash_duo.start_server(app)
+#
+#     home_content = display_page('home_content')
+#     app.layout.children[5] = home_content
+#
+#     driver.get(dash_duo.server_url+"/")
+#     dash_duo.wait_for_page(dash_duo.server_url+"/homepage",timeout=100)
+#     content_div = driver.find_element(by=By.ID, value='page-content')
+#
+#     print('======== start home content ============')
+#     print(content_div)
+#     print(content_div.get_attribute("innerHTML"))
+#     # driver.execute_script("arguments[0].innerHTML =" + str(home_content), content_div)
+#     # content_div = home_content
+#     # print(content_div)
+#     # upload_data_field = \
+#     #         driver.find_element(
+#     #             by=By.XPATH,
+#     #             value='//*[@id="upload-data"]/div/input')
+#     # mock_filename = os.path.join(os.getcwd(), 'mock_sqis.csv')
+#     # upload_data_field.send_keys(mock_filename)
+#     print('=========end home content ===========')
+
+
+
+# def test_upload_data(dash_duo):
+#     app = import_app("vital_sqi.app.index")
+#     dash_duo.start_server(app)
+#     driver = dash_duo.driver
+#     driver.get(dash_duo.server_url)
+#     print('====================')
+#     print(dash_duo.server_url)
+#     WebDriverWait(driver).until(lambda x:
+#                                 x.find_element(
+#                                     by=By.XPATH,
+#                                     value='//*[@id="upload-data"]/div/input'))
+#     mock_filename = os.path.join(os.getcwd(), 'mock_sqis.csv')
+#     upload_data_field = \
+#         driver.find_element(
+#             by=By.XPATH,
+#             value='//*[@id="upload-data"]/div/input')
+#     upload_data_field.send_keys("../test_data/mock_sqis.csv")
+#     assert True
+
+
+# from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+#
+# driver = Chrome()
+#
+# dc = DesiredCapabilities.CHROME
+# driver.capabilities['loggingPrefs'] = { 'browser':'ALL' }
+# driver.caps['goog:loggingPrefs'] = { 'browser':'ALL' }
+#
+# # mock_app = import_app('vital_sqi.app.views.dashboard2')
+#
+# driver.get('http://127.0.0.1:8050/')
+# driver.start_client()
+# mock_filename = 'mock_sqis.csv'
+# upload_data_field = \
+#                 driver.find_element(
+#                 by=By.ID,
+#                 value='confirm-rule-button')
+# upload_data_field.send_keys(os.path.join(os.getcwd(),'mock_sqis.csv'))
+#
+# print(driver.get_log('browser'))
+# print(driver.get_log('driver'))
+
+# upload_data_field = \
+#                 driver.find_element(
+#                 by=By.XPATH,
+#                 value='//*[@id="dashboard-content"]/store')
+# sleep(4)
+# print('donnneeee')
+# homepage_content = display_page('/homepagez')
+# mock_app = dash.Dash(__name__)
+# mock_app.layout = homepage_content
+# mock_app.run_server(debug=True)
+#
+# driver = webdriver.Chrome()
+# driver.get('http://127.0.0.1:8050/')
+# upload_data_field = \
+#                 driver.find_element(
+#                 by=By.XPATH,
+#                 value='//*[@id="upload-data"]/div/input')
+# mock_filename = 'mock_sqis.csv'
+# driver = webdriver.Chrome()
+# driver.get('http://127.0.0.1:8050/')
+# app = import_app('vital_sqi.app.index')
+# home_content = display_page('')
+# content_div = driver.find_element(by=By.ID,value='page-content')
+#
+# content_div = home_content
+# upload_data_field = driver.find_element(by=By.XPATH,
+#                     value='//*[@id="upload-data"]/div/input')
+# mock_filename = os.path.join(os.getcwd(),'mock_sqis.csv')
+# upload_data_field.send_keys(mock_filename)
+#
+# print('ahihi')
+# server = app.server
+# chrome.get(app)
+# print(chrome)
